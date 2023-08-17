f8fc841f58dfb03d80d85945da39334b6b00d4b9
robbie <robbiecsumner@gmail.com>
2023-08-17 21:54:19 +0200

    happy and sad images with base64

---
+++ src/MoodNFT.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MoodNFT is ERC721 {
+    using Counters for Counters.Counter;
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    Counters.Counter private tokenIdCounter;
+    string private s_happySvgImageURI;
+    string private s_sadSvgImageURI;
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory happySvgImageURI,
+        string memory sadSvgImageURI
+    ) ERC721("Mood NFT", "MOOD") {
+        s_happySvgImageURI = happySvgImageURI;
+        s_sadSvgImageURI = sadSvgImageURI;
+    }
+
+    function mint() public {
+        uint256 tokenId = tokenIdCounter.current();
+        tokenIdCounter.increment();
+        s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        Mood mood = s_tokenIdToMood[tokenId];
+        string memory moodString;
+        string memory imageURI;
+
+        if (mood == Mood.HAPPY) {
+            moodString = "happy";
+            imageURI = s_happySvgImageURI;
+        } else if (mood == Mood.SAD) {
+            moodString = "sad";
+            imageURI = s_sadSvgImageURI;
+        }
+
+        bytes memory tokenMetaData = abi.encodePacked(
+            '{"name": "',
+            name(),
+            '", "description": "This reflects the owners current mood.", attributes": [{"trait_type": "mood", "value": "',
+            moodString,
+            '"}], "image": "',
+            imageURI,
+            '"}'
+        );
+        string memory tokenMetaDataBase64 = Base64.encode(tokenMetaData);
+
+        return string(abi.encodePacked(_baseURI(), tokenMetaDataBase64));
+    }
+}
